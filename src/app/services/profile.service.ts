cc6dafdd85889332524bb4c5263088ee815f0c5a
Anthony Muli <mulianthony561@gmail.com>
2020-01-20 15:28:04 +0300

    added the access_token from antomuli's github account to enable the fetching of data from other github users

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,27 +1,35 @@
+import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-// import { Profile } from './profile';
-import 'rxjs/add/operator/map';
+ import { Profile } from './profile';
+import 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  // updateProfile(username: string) {
+  //   throw new Error("Method not implemented.");
+  // }
   
   private username:string;
-access_token = '2c9426754e2a96d7987835c686cf88beca8be7f9';
+  private access_token :string= '2c9426754e2a96d7987835c686cf88beca8be7f9';
+  private baseUrl:string = "https://api.github.com/users/"
   
   constructor(private http:HttpClient) {
     console.log("service is now ready");
-    this.username ="antomuli";
+    this.username ="antomuli"
    }
    getProfileInfo() {
-     return this.http.get( "https://api.github.com/users/access_token=" + this.access_token)
-     .map(res => res.json()); 
+     return this.http.get( this.baseUrl+this.username+"?access_token=" +this.access_token);
    }
 getProfileRepos() {
-  return this.http.get( "https://api.github.com/users/access_token=" + this.access_token)
-     .map(res => res.json()); 
+  return this.http.get( this.baseUrl+ this.username+ "/repos?access_token="+this.access_token);
+     
 }
+updateProfile(username:string) {
+  this.username=username;
+}
+
 }
 
